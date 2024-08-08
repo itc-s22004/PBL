d56bb3bb4f61177c783cce9dcdfa51d5699682e2
itc-s22007 <s22007@std.it-college.ac.jp>
2024-08-08 15:42:10 +0900

    削除

--- app/components/slideInPanel.js
+++ app/components/slideInPanel.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/SlideInPanel.css';
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { db } from '../database/firebase.js';
 
-const SlideInPanel = ({ isVisible, onClose, events }) => {
+const SlideInPanel = ({ isVisible, onClose, events, onEventDelete }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (isVisible) {
+      const fetchData = async () => {
+        const postData = collection(db, "schedules");
+        const snapshot = await getDocs(postData);
+        const docData = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          docId: doc.id,
+        }));
+        setPosts(docData);
+      };
+
+      fetchData();
+    }
+  }, [isVisible]);
+
+  const handleDelete = async (docId) => {
+    try {
+      await deleteDoc(doc(db, "schedules", docId));
+      console.log("Document successfully deleted!");
+      setPosts(posts.filter(post => post.docId !== docId));
+      onEventDelete(docId);
+      alert("予定を削除しました");
+    } catch (error) {
+      console.error("Error removing document: ", error);
+    }
+  };
+
   if (!isVisible) {
     return null;
   }
@@ -16,6 +48,7 @@ const SlideInPanel = ({ isVisible, onClose, events }) => {
               <h3>{event.title}</h3>
               <p>開始時刻: {event.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
               <p>終了時刻: {event.end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+              <button onClick={() => handleDelete(event.docId)}>削除</button>
             </div>
           ))
         ) : (
@@ -27,3 +60,4 @@ const SlideInPanel = ({ isVisible, onClose, events }) => {
 };
 
 export default SlideInPanel;
+
